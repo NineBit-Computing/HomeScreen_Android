bedb6a7e2ab0bfeac1c31b68a54d26965f4f8c6c
Tanishq Jha <tanishq.jha@ninebit.in>
2024-06-11 16:01:42 +0530

    Integerate upload file preview and make API call to get ML response

--- app/src/main/java/com/example/homescreen/FileUploadScreen.java
+++ app/src/main/java/com/example/homescreen/FileUploadScreen.java
@@ -26,7 +26,8 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import android.view.View;
 
-
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FileUploadScreen extends AppCompatActivity {
     private static final int FILE_SELECT_CODE = 0;
@@ -38,9 +39,12 @@ public class FileUploadScreen extends AppCompatActivity {
     private LinearLayout uploadProgressSection;
     private ImageView previewImage;
     private TextView uploadProgressPercentage;
+    private TextView ml_response_text;
+
 
     // File URI
     private Uri fileUri;
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +63,9 @@ public class FileUploadScreen extends AppCompatActivity {
         uploadProgressPercentage = findViewById(R.id.upload_progress_percentage);
         uploadStatus = findViewById(R.id.upload_status);
         previewImage = findViewById(R.id.previewImage);
+        ml_response_text = findViewById(R.id.ml_response_text);
+
+
 
         chooseFileButton.setOnClickListener(v -> showFileChooser());
 
@@ -110,7 +117,7 @@ public class FileUploadScreen extends AppCompatActivity {
                 if (file.exists()) {
                     try {
                         // Use your API endpoint for uploading
-                        String signedUrl = "https://ninebit.s3.ap-south-1.amazonaws.com/IMG_20240423_174708.jpg?Content-Type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIA3D7KSU77YT66VCTN%2F20240606%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20240606T093732Z&X-Amz-Expires=600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEoaCmFwLXNvdXRoLTEiRjBEAiBmHyhXBT%2FpQ5fN1SmVGiJ96sf4KSHuKM3qv30HFDMhggIgW5HuRwMBeagLe92bx3oqqATOJpIqs2njyMkqziHw2%2F0qmwMI0%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARADGgw3NjQ0NTkyNjM5OTkiDMfHBe7e0qykENZhYSrvAsXNnzjd7JrT%2FOx5WZuCygd%2BEaOhREllTQUtTq33zhzH5t0YsM9B2vdsjzCYzzCjmt%2FRsUih%2BDVJ%2Fh0Uwy92NaomOI%2Btj%2FdklWnSFJTKdjOUXxe2LWUvR%2BSjBro1hpG88bfpsMt%2BrcjGTi6Bs65raA5r%2BeT3sILKYOoW%2FugjWN3pAml6bX1E7NfS%2B6o0chalacot1knp6ROQXr211tsjBDK9HHTQEGYfIL5A4J8mB7KtoksWyEoUc%2FTksszH5Xy%2FQKOTE41GLuS3yPPOSmpVZpT2vIURw5cmxt%2F8AeCRG2UqGN0DyxR4v%2Fl3xfvthliLC2jRp4ajGQE9W5suCiO3N30NON0gXapAQg%2FqaqWy4qfwAsLECKq3M0b6SXEYeBPyUPKgBgXub4eURgtRk0Euwg2ZqkN%2BdiSrTDi%2B3AAUfyEHKQ1AuLxIddLLGiaPGfL5s6%2BmTVGxpRBOWJPPBPzVU7Zs6EeJzGVq70FF3%2FlqlSAw24aGswY6ngGRp%2Ff3U6PMCQLg2pOeoBPkAf73mlPPg4Yh9WR67gxbUyWM3TBHcWExksmkridfMhWcok24y7F7U%2Fi9BgeJffb5rTVBiDD1dVxi1ZW%2BS8EQL1yDgC5garQnBf1ReBqJjIy9trePEAZwRFyU4RCFTyLBp60cd%2BwKO92YSZFofx2yNnL6uBWlGq8rEik46APL7LpCFXMmYCb23I9m%2BS1SkQ%3D%3D&X-Amz-Signature=4aee5595d6e96d1ef3a72024c8f3f0f29d8e3aed59286cd62ba89a7374905ebc&X-Amz-SignedHeaders=host";
+                      String signedUrl = "https://ninebit.s3.ap-south-1.amazonaws.com/312.jpg?Content-Type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIA3D7KSU77T7IBVQ5N%2F20240611%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20240611T101204Z&X-Amz-Expires=600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEML%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCmFwLXNvdXRoLTEiRzBFAiAwfgVxEfAp16HX%2Fd5JHyZ0Ujo6xiY5q6qEv8c4E4iO%2FwIhAIdj9IZUdA%2FZzraY1rrRVLAwsVjXiSvBYgyX%2B2S2nkKyKpIDCFsQAxoMNzY0NDU5MjYzOTk5IgxfLLBKFOH4frYNr7Eq7wIQlk82WY85w1FNcHhDZT9zn7hRVN94aToMQisMWeRFwXYthJQqxGKCLJbWXTQAOqeR6kcCA3TRpSKrCIdIt6qBqFyEy9pmxa2Jl2q0s2T7dGwEvOlc2F7VjjPdh0q5yL7tUHe4tsDOp4RBkb6%2FU%2BN0sip%2Ff2gDg%2BAIFnajRMIVGi2wKuctSTi2y9bk%2BV%2FWmNxSnc76IANv5UBwQbcgIcJuvHY3NbicSyWqKQJv6eFqZqF5FF4%2BOfrM9RiRYdw%2Fb64ugjR9jYeJJwXyYfpBvaQdJIBu4aH78kEp0iSTd1p50aHPRLvFTjhiwN%2FJ10n6vGSo2RF35nBtadJfe27vPRBUO0xLkibvHshezstIO8ddNvJQufZ7QTYSnZOV%2FGv8fIDtUkBQmvR6dAkSuVJaITWGiFOZ9SwFgl6opqqjORxd7vgEszx5Fl68%2FJMlt%2FNhM1GqLFPe92bsn9D1zwEWuwlg%2FhQixZnhqOmCzYjMednSMPTFoLMGOp0BvPzyrYOkl%2BQWztju6nBo%2F287Ssr86ilFo%2B7P9cIi4ESOrc8MO6kdfZmNOdk%2B8IddKBGhDExCSBo%2F67q3naDY90EfMh7%2FTogOd3UqKrsjdO596gteAVTShQqzIA4pApTA7be7894DboHCmyiugclG61XDtMocIxAb2aIMDQ19faO0x3LbWbFR0d31sd9uVbhLpUuXiKs1qk6a24o17A%3D%3D&X-Amz-Signature=54d29e8a4fc6fa5b7bd65f5c77f7fb3cf08352ae7c18c94dec3ebf4433d26760&X-Amz-SignedHeaders=host";
                         uploadToS3(file, signedUrl);
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -206,10 +213,67 @@ public class FileUploadScreen extends AppCompatActivity {
             public void onResponse(okhttp3.Call call, Response response) throws IOException {
                 if (response.isSuccessful()) {
                     Log.i("Upload", "SUCCESS");
+                    runOnUiThread(()-> uploadStatus.setText("Upload Successful"));
+                    getMLresponse(fileName);
                 } else {
                     Log.e("Upload", "ERROR: " + response.message());
+                    runOnUiThread(()-> uploadStatus.setText("Upload Failed"));
                 }
             }
         });
     }
+
+    private void getMLresponse(String fileName) {
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://apis-dev.ninebit.in/py/v1/classify-image";
+        String json = " {\n" +
+                "    \"object\" : \"312.jpg\",\n" +
+                "    \"model\": \"model1.h5\"\n" +
+                "} ";
+        RequestBody requestBody = RequestBody.create(json, MediaType.parse("application/json"));
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+                Log.e("ML Response", "ERROR", e);
+                runOnUiThread(() -> uploadStatus.setText("ML Response Failed"));
+            }
+
+            @Override
+            public void onResponse(okhttp3.Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+                    try {
+                        JSONObject jsonResponse = new JSONObject(responseBody);
+                        JSONObject message = jsonResponse.getJSONObject("message");
+                        JSONObject body = new JSONObject(message.getString("body"));
+
+                        // Getting the "dRes" object
+                        JSONObject dRes = body.getJSONObject("dRes");
+
+                        // Getting the "emotion" value
+                        String emotion = dRes.getString("emotion");
+
+                        Log.i("Emotion", emotion);
+                        runOnUiThread(() -> {
+                            ml_response_text.setText("Emotion: " + emotion);
+                            ml_response_text.setVisibility(View.VISIBLE);
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        runOnUiThread(() -> uploadStatus.setText("ML Response: Failed (JSONException)"));
+                    }
+                } else {
+                    Log.e("ML Response", "ERROR: " + response.message());
+                    runOnUiThread(() -> uploadStatus.setText("ML Response Failed"));
+                }
+            }
+        });
+    }
+
+
 }
